c50ebfd66856c95e8fd09cd827d6f68010220f85
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-02 01:27:53 +0200

    change constraint sql

--- src/database/config/build.sql
+++ src/database/config/build.sql
@@ -5,14 +5,14 @@ DROP TABLE IF EXISTS authors CASCADE;
 
 CREATE TABLE authors (
     id INT PRIMARY KEY ,
-    name VARCHAR(125) NOT NULL,
+    name VARCHAR(125) ,
     location VARCHAR(125) 
 );
 
 CREATE TABLE books (
     id SERIAL PRIMARY KEY ,
-    name VARCHAR(125) NOT NULL,
-    release_date VARCHAR(100) NOT NULL,
+    name VARCHAR(125) ,
+    release_date VARCHAR(100) ,
     author_id INT references authors(id)
 );
 
