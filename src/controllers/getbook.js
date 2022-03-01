9290e02d7c55e4ac6320c48ad41bdca9f674f637
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-02 01:36:36 +0200

    get data from database

--- src/controllers/getbook.js
+++ src/controllers/getbook.js
@@ -1,14 +1,10 @@
-
 const getData = require('../database/queries/getData');
 
 const getbook = (req, res) => {
   getData()
-  .then(data =>{
-    console.log(data.rows);
-    res.json(data.rows);
-  })
-  
-}
+    .then((data) => {
+      console.log(data.rows);
+      res.json(data.rows);
+    });
+};
 module.exports = getbook;
-
-
--- src/database/queries/getData.js
+++ src/database/queries/getData.js
@@ -1,9 +1,9 @@
 const connection = require('../config/connection');
 
-const getbooks = () =>{
-    const sql = {
-        text : `SELECT * from books ;` ,
-    }
-    return connection.query(sql)
+const getData = () => {
+  const sql = {
+    text: 'SELECT books.name as book, authors.name as author, books.release_date as date from books join authors on(books.author_id = authors.id);',
+  };
+  return connection.query(sql);
 };
-module.exports = getbooks;
+module.exports = getData;
