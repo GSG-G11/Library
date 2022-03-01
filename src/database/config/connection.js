30ef79acda38ba3a839197b851d9f6c7a8df8ad6
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-02 01:31:43 +0200

    change use eslint

--- src/database/config/connection.js
+++ src/database/config/connection.js
@@ -1,13 +1,13 @@
-require ('env2')('.env');
-const {Pool} = require('pg');
+require('env2')('.env');
+const { Pool } = require('pg');
 
-if(!process.env.DB_URL){
-    throw new Error ("DB URL doesn't exise");
-};
+if (!process.env.DB_URL) {
+  throw new Error("DB URL doesn't exise");
+}
 
 const connection = new Pool({
-    connectionString : process.env.DB_URL ,
-    ssl : false,
+  connectionString: process.env.DB_URL,
+  ssl: false,
 });
 
 module.exports = connection;
