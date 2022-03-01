44e937a6de03377a0eaf1bcda7685fba54fc5715
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-02 01:39:45 +0200

    edit index.js and router.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,9 @@
 const homePage = require('./homePage');
 const getbook = require('./getbook');
+const addbook = require('./addbook');
 
-module.exports = { homePage , getbook};
+module.exports = {
+  homePage,
+  getbook,
+  addbook,
+};
