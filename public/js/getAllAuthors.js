e17cb7cc53cd27c2db9421b822ff53b0dbdba80f
yousra Jaradh <yousrakhaleel@gmail.com>
2022-03-02 16:09:57 +0300

    edit authors option Relates #16

--- public/js/getAllAuthors.js
+++ public/js/getAllAuthors.js
@@ -4,10 +4,13 @@ fetch('/authors')
 .then((res) => {
     res.json()
     .then((data) => {
+        console.log(data);
         data.forEach(element => {
             const authorName = document.createElement('option');
             authorName.value = element.id;
             authorName.textContent = element.name;
+
+            author.appendChild(authorName);
         });
     })
 })
