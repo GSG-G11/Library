8299a8b9fa54b9370f4a585356da461dac16f0d1
yousra Jaradh <yousrakhaleel@gmail.com>
2022-03-02 11:46:27 +0300

    delete-data Relates #8

--- src/database/queries/getData.js
+++ src/database/queries/getData.js
@@ -2,7 +2,7 @@ const connection = require('../config/connection');
 
 const getData = () => {
   const sql = {
-    text: 'SELECT books.name as book, authors.name as author, books.release_date as date from books join authors on(books.author_id = authors.id);',
+    text: 'SELECT books.id , books.name as book, authors.name as author, books.release_date as date from books join authors on(books.author_id = authors.id);',
   };
   return connection.query(sql);
 };
