b60227e37e16c759bc552ac0f5750a182f359028
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-02 18:29:22 +0200

    edit test.js

--- src/test/test.js
+++ src/test/test.js
@@ -1,12 +1,7 @@
 const request = require('supertest');
 const app = require('../app');
 
-test("jest is working", () => {
-  expect(1).toBe(1);
-});
-
-describe('Test server endpoints', () => {
-  // Success root endpoint
+describe('Test server home page', () => {
   test('GET Route /, status 200, content-type html', (done) => {
     request(app)
       .get('/')
@@ -17,28 +12,26 @@ describe('Test server endpoints', () => {
         return done();
       });
   });
-});
 
-test('GET Route /, status 404, content-type html', (done) => {
-  request(app)
-    .get('/ss')
-    .expect(404)
-    .expect('Content-Type', /html/)
-    .end((err, res) => {
-      if (err) return done(err);
-      return done();
-    });
+  test('GET Route /ss, status 404, content-type html', (done) => {
+    request(app)
+      .get('/ss')
+      .expect(404)
+      .expect('Content-Type', /html/)
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+  test('POST Route /insertData, status 200, content-type json', (done) => {
+    request(app)
+      .post('/insertData')
+      .expect(200)
+      .expect('content-type', /json/)
+      .send({ book: 'book1', author: 'author1', date: '1/1/2022' })
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
 });
-
-test('POST Route /addNote, status 200, content-type json', (done) => {
-  request(app)
-    .post('/books')
-    .expect(302)
-    .expect('content-type', /text/)
-    .send({ book: 'book1', author: 'author1', date: '1/1/2022' })
-    .end((err, res) => {
-      if (err) return done(err);
-      return done();
-    });
-});
-
