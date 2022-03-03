8e26f6d205fe4f2b1e86b2ca4f044737b8719304
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-03-03 10:03:33 +0200

    deit authors name and books name

--- src/database/config/build.sql
+++ src/database/config/build.sql
@@ -18,24 +18,30 @@ CREATE TABLE books (
 
 INSERT INTO authors (id, name, location) VALUES
 (
-    1,'author1' , 'gaza'
+    1,'Khaled Hosseini' , 'gaza'
 ),
 (
-    2,'author2' , 'gaza'
+    2,'Mustafa Mahmoud' , 'gaza'
 ),
 (
-    3,'author3' , 'gaza'
+    3,'Ibrahim al-Fiqi' , 'gaza'
+),
+(
+    4,'William Shakespeare' , 'gaza'
+),
+(
+    5,'Franz Kafka' , 'gaza'
 );
 
 INSERT INTO books (name , release_date ,author_id) VALUES
 (
-    'book1' , '1/1/2022',1
+    'The kite runner' , '1/1/2022',1
 ),
 (
-    'book2' , '1/1/2022',2
+    'time management' , '1/1/2022',2
 ),
 (
-    'book3' , '1/1/2022',3
+    'King Lear' , '1/1/2022',3
 );
 
 COMMIT;
